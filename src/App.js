d519d69166cc567503c11f018e02ba06d5fee6d0
Chris Horne <chrhorne@deloitte.com>
2024-10-09 08:10:17 -0700

    comment out Login component for testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './Login';
+// import Login from './Login';
 import FraudResultsTable from './components/FraudResultsTable'; // Import the component
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<FraudResultsTable />} /> {/* Define the route */}
+      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/dashboard" element={<FraudResultsTable />} /> Define the route
     </Routes>
   );
 };
