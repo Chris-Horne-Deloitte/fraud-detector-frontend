c913258ca8fcfd9e110a4844e89a1b7d952404a3
Chris Horne <chrhorne@deloitte.com>
2024-10-09 08:36:40 -0700

    uncommented Login.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import Login from './Login';
+import Login from './Login';
 import FraudResultsTable from './components/FraudResultsTable'; // Import the component
 
 const App = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<FraudResultsTable />} /> Define the route
     </Routes>
   );
